1e0ad1f5e64c11dc25c7f58b7b8d68347c565f31
Ben Newman <ben@benjamn.com>
2018-01-29 11:43:24 -0500

    Fix tests in older Node versions.

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,13 @@
 import assert from "assert";
-import { Tuple, tuple } from "../src/tuple.js";
+import { Tuple, tuple } from "../dist/tuple.js";
 
 describe("Tuple basics", function () {
   it("should be defined and importable", function () {
     assert.strictEqual(typeof Tuple, "function");
     assert.strictEqual(typeof tuple, "function");
+  });
+
+  it("should support === deep equality", function () {
+    assert.strictEqual(tuple(1,2,3), new Tuple(1,2,3));
   });
 });

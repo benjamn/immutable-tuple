0297500cdfef039dc31fcac1f41dea6e1c3f3ef4
Ben Newman <ben@benjamn.com>
2018-01-29 12:11:07 -0500

    Add tests of Array methods.

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import assert from "assert";
 import { Tuple, tuple } from "../dist/tuple.js";
 
@@ -9,5 +11,168 @@ describe("Tuple basics", function () {
 
   it("should support === deep equality", function () {
     assert.strictEqual(tuple(1,2,3), new Tuple(1,2,3));
+  });
+});
+
+describe("Array methods", function () {
+  it("concat", function () {
+    assert.strictEqual(
+      tuple(1,2,3).concat(4, tuple(5,6), 7),
+      tuple(1,2,3,4,5,6,7)
+    );
+  });
+
+  it("slice", function () {
+    assert.strictEqual(
+      tuple(1,2,3,4,5).slice(2, 4),
+      tuple(3,4)
+    );
+
+    assert.strictEqual(
+      tuple(1,2,3,4,5).slice(-3),
+      tuple(3,4,5)
+    );
+  });
+
+  it("includes", function () {
+    const t = tuple(1, "asdf", true);
+    assert.strictEqual(t.includes("asdf"), true);
+    assert.strictEqual(t.includes(false), false);
+  });
+
+  it("indexOf and lastIndexOf", function () {
+    const t = tuple(1, 1, 2, 3, 5, 8, 13);
+    assert.strictEqual(t.indexOf(1), 0)
+    assert.strictEqual(t.lastIndexOf(1), 1)
+    assert.strictEqual(t.indexOf(4), -1)
+    assert.strictEqual(t.lastIndexOf(7), -1)
+  });
+
+  it("forEach", function () {
+    const output = {};
+    tuple("a", "b", "c", "d").forEach(function (item, i) {
+      output[item] = i;
+    });
+    assert.deepEqual(output, {
+      a: 0,
+      b: 1,
+      c: 2,
+      d: 3
+    });
+  });
+
+  it("filter", function () {
+    assert.strictEqual(
+      tuple(1,2,3,4,5,6,7,8,9).filter(x => x % 2),
+      tuple(1,3,5,7,9)
+    );
+  });
+
+  it("map", function () {
+    assert.strictEqual(
+      tuple(1,2,3,4).map(x => x + 1),
+      tuple(2,3,4,5)
+    );
+  });
+
+  it("every", function () {
+    assert.strictEqual(tuple(2,4,6,8).every(x => x % 2 === 0), true);
+    assert.strictEqual(tuple(2,4,6,7).every(x => x % 2 === 0), false);
+  });
+
+  it("some", function () {
+    assert.strictEqual(tuple(1,2,3,4).some(x => x === 3), true);
+    assert.strictEqual(tuple(1,2,3,4).some(x => x > 5), false);
+  });
+
+  it("reduce", function () {
+    assert.strictEqual(
+      tuple(1,2,3,4,5).reduce((x, sum) => x + sum, 0),
+      15
+    );
+  });
+
+  it("reduceRight", function () {
+    assert.strictEqual(
+      [tuple(0, 1), tuple(2, 3), tuple(4, 5)].reduceRight(
+        (previous, current) => previous.concat(current)),
+      tuple(4, 5, 2, 3, 0, 1)
+    );
+  });
+
+  it("toString", function () {
+    assert.strictEqual(
+      String(tuple(1,2,3)),
+      [1,2,3].toString()
+    );
+  });
+
+  it("join", function () {
+    assert.strictEqual(tuple(1,2,3).join("|"), "1|2|3");
+  });
+
+  it("reverse", function () {
+    assert.strictEqual(
+      tuple(1,2,3).reverse(),
+      tuple(3,2,1)
+    );
+  });
+
+  it("sort", function () {
+    assert.strictEqual(
+      tuple(4,2,7,6,9,3,1,0,3,2,7).sort(),
+      tuple(0,1,2,2,3,3,4,6,7,7,9)
+    );
+  });
+
+  it("find", function () {
+    assert.deepEqual(
+      tuple({
+        foo: 1
+      }, {
+        bar: 2
+      }, {
+        baz: 3
+      }, {
+        qux: 4
+      }).find(obj => {
+        return Object.keys(obj).some(key => {
+          return key.length === obj[key];
+        });
+      }),
+      { baz: 3 }
+    );
+  });
+
+  it("findIndex", function () {
+    assert.deepEqual(
+      tuple({
+        foo: 1
+      }, {
+        bar: 2
+      }, {
+        baz: 3
+      }, {
+        qux: 4
+      }).findIndex(obj => {
+        return Object.keys(obj).some(key => {
+          return key.length === obj[key];
+        });
+      }),
+      2
+    );
+  });
+
+  it("Symbol.iterator", function () {
+    assert.deepEqual(
+      [2, 4, 6, 8],
+      [...tuple(1, 2, 3, 4, 5, 6, 7, 8, 9).filter(x => x % 2 === 0)]
+    );
+
+    const [a, b, c, ...rest] = tuple(1, 2, 3, 4, 5);
+    assert.strictEqual(a, 1);
+    assert.strictEqual(b, 2);
+    assert.strictEqual(c, 3);
+    assert.deepEqual(rest, [4, 5]);
   });
 });
